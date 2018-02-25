58486a8d40389eb5b35f364b2c1bb6d8512a63a6
Sergii Ovcharenko <sergii@sovcharenko.com>
2018-02-25 19:53:30 +0000

    Better tests

--- spec/specs/get_xcconfig_value_action_spec.rb
+++ spec/specs/get_xcconfig_value_action_spec.rb
@@ -18,27 +18,20 @@ describe Fastlane::Actions::GetXcconfigValueAction do
     end
 
     it 'returns value' do
-      expect(get_xcconfig_value('name1')).to eq('value1')
+      {
+        'EMPTY' => '',
+        'ARCHS' => '$(ARCHS_STANDARD)',
+        'CLANG_WARN_EMPTY_BODY' => 'YES',
+        'PRODUCT_BUNDLE_IDENTIFIER' => 'com.sovcharenko.App',
+        'ONLY_ACTIVE_ARCH[config=Debug][sdk=*][arch=*]' => 'YES',
+        'ONLY_ACTIVE_ARCH[config=Release]' => 'NO'
+      }.each do |key, value|
+        expect(get_xcconfig_value(key)).to eq(value)
+      end
     end
 
-    it 'returns value (subscript)' do
-      expect(get_xcconfig_value('name2[sub1=x86][sub2=os]')).to eq('value2')
-    end
-
-    it 'strips out comments and returns value' do
-      expect(get_xcconfig_value('name3')).to eq('value3')
-    end
-
-    it 'reads empty value' do
-      expect(get_xcconfig_value('name5')).to eq('')
-    end
-
-    it 'reads and trims name and value' do
-      expect(get_xcconfig_value('name6')).to eq('value  6')
-    end
-
-    it 'doesn\'t read invalid settings'  do
-      ['iname1', 'iname2[]', 'iname//3', 'iname4'].each do |name|
+    it 'ignores invalid settings' do
+      ['1FOO', 'FOO', 'FOO//BAR', 'FOO BAR'].each do |name|
         expect { get_xcconfig_value(name) }.to raise_exception do |exception|
           expect(exception.message).to start_with("Couldn't read '#{name}' from")
         end
--- spec/specs/set_xcconfig_value_action_spec.rb
+++ spec/specs/set_xcconfig_value_action_spec.rb
@@ -10,30 +10,26 @@ end
 
 describe Fastlane::Actions::SetXcconfigValueAction do
   describe '#run' do
-    before(:each) do
-      allow(FastlaneCore::Helper).to receive(:sh_enabled?).and_return(true)
-      @tmp_dir = Dir.mktmpdir('fastlane-plugin-xcconfig ')
-    end
-
-    after(:each) do
-      FileUtils.rm_rf(@tmp_dir)
-    end
-
     def set_xcconfig_value(name, value, file = "set.xcconfig")
-      path = File.join(File.dirname(__FILE__), "fixtures/#{file}")
-      before_content = nil
+      tmp_dir = Dir.mktmpdir('fastlane-plugin-xcconfig ')
+      begin
+        path = File.join(File.dirname(__FILE__), "fixtures/#{file}")
+        before_content = nil
 
-      if File.exist?(path)
-        FileUtils.cp(path, @tmp_dir)
-        path = File.join(@tmp_dir, file)
-        before_content = Xcodeproj::Config.new(path)
+        if File.exist?(path)
+          FileUtils.cp(path, tmp_dir)
+          path = File.join(tmp_dir, file)
+          before_content = Xcodeproj::Config.new(path)
+        end
+
+        SpecHelper::FastlaneHelper.execute_as_lane("set_xcconfig_value(path:'#{path}', name: '#{name}', value: '#{value}')")
+
+        after_content = Xcodeproj::Config.new(Xcodeproj::Config.new(path))
+
+        [before_content, after_content]
+      ensure
+        FileUtils.rm_rf(tmp_dir)
       end
-
-      SpecHelper::FastlaneHelper.execute_as_lane("set_xcconfig_value(path:'#{path}', name: '#{name}', value: '#{value}')")
-
-      after_content = Xcodeproj::Config.new(Xcodeproj::Config.new(path))
-
-      [before_content, after_content]
     end
 
     it 'raises error when file doesn\'t exist' do
@@ -49,40 +45,17 @@ describe Fastlane::Actions::SetXcconfigValueAction do
     end
 
     it 'updates value' do
-      before, after = set_xcconfig_value('name1', 'new_value')
-      expect(after.attributes['name1']).to eq('new_value')
-      expect(after.attributes.except!('name1')).to eq(before.attributes.except!('name1'))
-    end
-
-    it 'updates value (subscript)' do
-      before, after = set_xcconfig_value('name2[sub1=x86][sub2=os]', 'new_value')
-      expect(after.attributes['name2[sub1=x86][sub2=os]']).to eq('new_value')
-      expect(after.attributes.except!('name2[sub1=x86][sub2=os]')).to eq(before.attributes.except!('name2[sub1=x86][sub2=os]'))
-    end
-
-    it 'strips out comments and updates value' do
-      before, after = set_xcconfig_value('name3', 'new_value')
-      expect(after.attributes['name3']).to eq('new_value')
-      expect(after.attributes.except!('name3')).to eq(before.attributes.except!('name3'))
-    end
-
-    it 'updates empty value' do
-      before, after = set_xcconfig_value('name5', 'new_value')
-      expect(after.attributes['name5']).to eq('new_value')
-      expect(after.attributes.except!('name5')).to eq(before.attributes.except!('name5'))
-    end
-
-    it 'trims name and updates value' do
-      before, after = set_xcconfig_value('name6', 'new_value')
-      expect(after.attributes['name6']).to eq('new_value')
-      expect(after.attributes.except!('name6')).to eq(before.attributes.except!('name6'))
-    end
-
-    it 'doesn\'t update invalid settings' do
-      ['iname1', 'iname2[]', 'iname//3', 'iname4'].each do |name|
-        expect { set_xcconfig_value(name, 'new_value') }.to raise_exception do |exception|
-          expect(exception.message).to start_with("Couldn't find '#{name}' in")
-        end
+      {
+        'EMPTY' => 'new_value',
+        'ARCHS' => 'nil',
+        'CLANG_WARN_EMPTY_BODY' => 'NO',
+        'PRODUCT_BUNDLE_IDENTIFIER' => 'com.sovcharenko.App-beta',
+        'ONLY_ACTIVE_ARCH[config=Debug][sdk=*][arch=*]' => 'NO',
+        'ONLY_ACTIVE_ARCH[config=Release]' => 'YES'
+      }.each do |key, value|
+        before, after = set_xcconfig_value(key, value)
+        expect(after.attributes[key]).to eq(value)
+        expect(after.attributes.except!(key)).to eq(before.attributes.except!(key))
       end
     end
   end
