e076127ce471211d903c665b5d1bfb1c5ec89764
Ste Prescott <ste.prescott@sky.uk>
2019-03-25 22:38:00 +0000

    Add update_xcconfig_value method to keep previous feature

---
+++ spec/specs/update_xcconfig_value_action_spec.rb
@@ -0,0 +1,62 @@
+require 'xcodeproj'
+
+# https://stackoverflow.com/questions/6227600/how-to-remove-a-key-from-hash-and-get-the-remaining-hash-in-ruby-rails
+class Hash
+  def except!(*keys)
+    keys.each { |key| delete(key) }
+    self
+  end
+end
+
+describe Fastlane::Actions::UpdateXcconfigValueAction do
+  describe '#run' do
+    def update_xcconfig_value(name, value, file = "update.xcconfig")
+      tmp_dir = Dir.mktmpdir('fastlane-plugin-xcconfig ')
+      begin
+        path = File.join(File.dirname(__FILE__), "fixtures/#{file}")
+        before_content = nil
+
+        if File.exist?(path)
+          FileUtils.cp(path, tmp_dir)
+          path = File.join(tmp_dir, file)
+          before_content = Xcodeproj::Config.new(path)
+        end
+
+        SpecHelper::FastlaneHelper.execute_as_lane("update_xcconfig_value(path:'#{path}', name: '#{name}', value: '#{value}')")
+
+        after_content = Xcodeproj::Config.new(Xcodeproj::Config.new(path))
+
+        [before_content, after_content]
+      ensure
+        FileUtils.rm_rf(tmp_dir)
+      end
+    end
+
+    it 'raises error when file doesn\'t exist' do
+      expect { update_xcconfig_value('name1', 'new_value', 'non_existent_file.xcconfig') }.to raise_exception do |exception|
+        expect(exception.message).to start_with('Couldn\'t find xcconfig file at path')
+      end
+    end
+
+    it 'raises error when setting couldn\'t be found' do
+      expect { update_xcconfig_value('some_name', 'new_value') }.to raise_exception do |exception|
+        expect(exception.message).to start_with('Couldn\'t find \'some_name\' in')
+      end
+    end
+
+    it 'updates value' do
+      {
+        'EMPTY' => 'new_value',
+        'ARCHS' => 'nil',
+        'CLANG_WARN_EMPTY_BODY' => 'NO',
+        'PRODUCT_BUNDLE_IDENTIFIER' => 'com.sovcharenko.App-beta',
+        'ONLY_ACTIVE_ARCH[config=Debug][sdk=*][arch=*]' => 'NO',
+        'ONLY_ACTIVE_ARCH[config=Release]' => 'YES'
+      }.each do |key, value|
+        before, after = update_xcconfig_value(key, value)
+        expect(after.attributes[key]).to eq(value)
+        expect(after.attributes.except!(key)).to eq(before.attributes.except!(key))
+      end
+    end
+  end
+end
